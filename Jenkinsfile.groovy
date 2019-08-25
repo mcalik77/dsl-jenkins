381a51e75f0c5a90e470e9bbdeab8266e4e43c44
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-25 13:29:46 -0700

    edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -34,8 +34,8 @@ pipeline{
         stage("Write to a file"){
             steps{
                 ws("tmp/"){
-                    writeFile text: "Test", file "Testfile"
-              
+                    writeFile text: "Test"\n "line2", file: "TestFile"
+                    sh "cat TestFile"
                 }
             }
         }
