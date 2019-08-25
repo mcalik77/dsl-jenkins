1792dd50f02786bb60896e2908313549e41651be
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-25 13:08:10 -0700

    edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,12 +6,9 @@ pipeline{
                 sh '''
                 set +xe
                 echo Hello
-                ech Error
-                sudo yum install httpd -y
-                sudo yum install wget -y
-                sudo yum insatll unzip -y
+                ech  Error
+                sudo yum install httpd wget unzip -y
                 ping -c 4 google.com
-
                 '''
             }
         }
@@ -19,12 +16,18 @@ pipeline{
             steps{
                 ws("tmp/"){
                     sh "pwd"
-                    sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                    sh "unzip terraform_0.12.7_linux_amd64.zip"
+                    def exists fileExists 'terraform_0.12.7_linux_amd64.zip'
+                    if (exists){
+                        sh "unzip terraform_0.12.7_linux_amd64.zip"
+                    }
+                    else{
+                        sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                    }
+                    sh "unzip terraform_0.12.7_linux_amd64.zip"          
+                    sh "sudo mv terraform /bin"
+                    sh "terraform version"
                 }
             }
-
         }
     }
 }
-
