37218b65a10d370d4e1d3dd4f35b4208af573a94
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-25 13:15:40 -0700

    edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,11 +19,11 @@ pipeline{
                         def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
                         if (exists) {
                             sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                            sh "mv terraform /bin"
+                            sh "sudo mv terraform /bin"
                             sh "terraform version"
                         } else {
                             sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                            sh "mv terraform /bin"
+                            sh "sudo mv terraform /bin"
                             sh "terraform version"
                         }
                     }
