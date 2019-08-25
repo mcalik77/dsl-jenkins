752ff92f5ca094a9f3f24884becd019436762784
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-25 12:58:55 -0700

    edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,9 +19,11 @@ pipeline{
                 ws("tmp/"){
                     sh "pwd"
                     sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                    sh "unzip terraform_0.12.7_linux_amd64.zip"
                 }
             }
 
         }
     }
 }
+
