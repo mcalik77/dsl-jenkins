3d2f03309240ac1c0989e1b5434904541bc6bf9d
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-25 12:42:05 -0700

    added code

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,12 @@ pipeline{
     stages{
         stage("Run Command"){
             steps{
-                sh "echo Hello"
+                sh '''
+                echo Hello
+                yum install httpd -y
+                ping -c 4 google.com
+
+                '''
             }
         }
     }
