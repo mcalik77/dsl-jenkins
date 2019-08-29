81ea44e06b32d0b7b10b87ca8c0c093210d84677
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-28 18:42:26 -0700

    added older terraform

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,15 +16,15 @@ pipeline{
             steps{
                 ws("tmp/"){
                     script {
-                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                        def exists = fileExists 'terraform_0.11.9_linux_amd64.zip'
                         if (exists) {
-                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                            sh "sudo mv terraform /bin"
+                            sh "unzip -o terraform_0.11.9_linux_amd64.zip"
+                            sh "sudo mv -f terraform /bin"
                             sh "terraform version"
                         } else {
-                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                            sh "sudo mv terraform /bin"
+                            sh "wget https://releases.hashicorp.com/terraform/0.11.9/terraform_0.11.9_linux_amd64.zip"
+                            sh "unzip -o terraform_0.11.9_linux_amd64.zip"
+                            sh "sudo mv -f terraform /bin"
                             sh "terraform version"
                         }
                     }
